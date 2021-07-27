7fd05eea07b34cb91b75cb7233768750bcd011c9
putriharviana <putriharviana@gmail.com>
2021-07-27 21:36:24 +0700

    fix issue router

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueRouter from 'vue-router'
+import router from '../src/router/index'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
 new Vue({
+  el: '#app',
+  router,
   render: h => h(App),
 }).$mount('#app')
